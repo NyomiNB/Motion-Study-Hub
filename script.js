4baf6208f5ad8b11416085181e51b90a990c2343
AaroheeD <aaroheedabas@gmail.com>
2024-09-02 21:03:48 +0000

    light mode dark mode

---
+++ script.js
@@ -0,0 +1,19 @@
+var body = $("body")
+// var text = document.getElementById("welcome");
+
+var light = $(".lightmode")
+
+light.on("click" , lightfunction)
+
+function lightfunction(){
+  body.css("background" , "white")
+}
+
+//dark mode
+var dark = $(".darkmode")
+dark.on("click" , darkfunction)
+
+function darkfunction(){
+  body.css("background" , "black")
+  // text.color = "yellow";
+}
